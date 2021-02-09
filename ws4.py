18c8d275c5e89374c25f05e8ea46dcd714d71edc
adoti <34661902+adoti@users.noreply.github.com>
2021-02-08 20:54:33 -0500

    added init()
    moved most of main prog into init()
    
    next update needs to rename ws4.py to main.py

--- ws4.py
+++ ws4.py
@@ -2,20 +2,23 @@ import websocket_manager
 import tkinter as tk
 
 ws = websocket_manager.WebsocketManager()
+window = tk.Tk()
+price_p = tk.Label(text=str(0))
+price_p.pack()
 
 def update():
 	price_p.config(text=str(ws.msg))
 	window.after(200,update)
 
+def init():
+	ws.connect()
+	cnx_end = tk.Button(window, command = ws.close_connection, text = "close connection")
+	cnx_end.pack()
+	window.after(200, update)
 #connect should actually activate on button press
-ws.connect()
 
-window = tk.Tk()
-price_p = tk.Label(text=str(0))
-price_p.pack()
-cnx_end = tk.Button(window, command = ws.close_connection, text = "close connection")
-cnx_end.pack()
+init()
+window.mainloop()
+
 #here, we'll add all the elements to the window
 #we'll update them all in the update() function
-window.after(200,update)
-window.mainloop()
