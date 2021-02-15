5eeb7308b91210f3b2d331ef639b308029acb290
adoti <34661902+adoti@users.noreply.github.com>
2021-02-14 23:12:50 -0500

    trade size added
    added target & exit text boxes to main window.  will change ws4.py to something like "main.py" soon.  created trade class that handles trade sizing based on USD risk, calculating actual r, created a round down function.  will eventually keep the possible trades in a separate file to be referenced which will store the max decimal places for each.  Trade class will calculate a position size and then recalculate the risk/reward by the truncated position size.

---
+++ trade_sizer.py
@@ -0,0 +1,124 @@
+import math
+
+def round_d(number:float, decimals:int=2):
+    """
+    Returns a value rounded down to a specific number of decimal places.
+    """
+    if not isinstance(decimals, int):
+        raise TypeError("decimal places must be an integer")
+    elif decimals < 0:
+        raise ValueError("decimal places has to be 0 or more")
+    elif decimals == 0:
+        return math.floor(number)
+
+    factor = 10 ** decimals
+    return math.floor(number * factor) / factor
+
+class Trade:
+	_taker_fee = 0.0007
+	_maker_fee = 0.0002
+
+	def __init__(self, entry, target, exit, risk):
+		self.entry = entry
+		self.target = target
+		self.exit = exit
+		self.risk = risk
+
+		if self.entry > self.exit:
+			self.direction = "LONG"
+		elif self.entry < self.exit:
+			self.direction = "SHORT"
+		else:
+			self.direction = None
+
+	def update(self, entry, target, exit, risk):
+		self.entry = entry
+		self.target = target
+		self.exit = exit
+		self.risk = risk
+
+		if self.entry > self.exit:
+			self.direction = "LONG"
+		elif self.entry < self.exit:
+			self.direction = "SHORT"
+		else:
+			self.direction = None
+
+	#risk is defined as a % of the bankroll to be risked. we can double check it though.
+	def get_dir(self):
+		return self.direction
+
+	def get_rr(self):
+		return -1*self.target_return()/self.exit_return()
+
+	def get_position_size(self):
+		if self.direction:
+			if self.direction == "LONG":
+				return round_d(self.risk/(self.exit - self.entry - self.market_fee(self.exit) - self.market_fee(self.entry)),4)
+			else:
+				return round_d(self.risk/(self.entry - self.exit - self.market_fee(self.exit) - self.market_fee(self.entry)),4)
+		return None
+		# if entry_usd > exit_usd: #the trade is LONG
+		# 	return ("Long",round(target_risk_usd/(exit_usd - entry_usd - market_fee(entry_usd) - market_fee(exit_usd)),4))
+		# else: #the trade is SHORT
+		# 	return ("Short",round(target_risk_usd/(entry_usd - exit_usd - market_fee(entry_usd) - market_fee(exit_usd)),4))
+
+	def target_return(self):
+		if self.direction:
+			if self.direction == "LONG":
+				return self.get_position_size() * (self.target - self.entry - self.limit_fee(self.target) - self.market_fee(self.entry))
+			else:
+				return self.get_position_size() * (self.entry - self.target - self.limit_fee(self.target) - self.market_fee(self.entry))
+		return None
+
+	#aka get actual risk
+	def exit_return(self):
+		if self.direction:
+			if self.direction == "LONG":
+				return self.get_position_size() * (self.exit - self.entry - self.limit_fee(self.exit) - self.market_fee(self.entry))
+			else:
+				return self.get_position_size() * (self.entry - self.exit - self.limit_fee(self.exit) - self.market_fee(self.entry))
+		return None
+
+	def market_fee(self, price):
+		return price * self._taker_fee
+
+	def limit_fee(self, price):
+		return price * self._maker_fee
+
+	#assumes risk = negative number
+	def get_position_size(self):
+		if self.direction:
+			if self.direction == "LONG":
+				return round_d(self.risk/(self.exit - self.entry - self.market_fee(self.exit) - self.market_fee(self.entry)),4)
+			else:
+				return round_d(self.risk/(self.entry - self.exit - self.market_fee(self.exit) - self.market_fee(self.entry)),4)
+		return None
+		# if entry_usd > exit_usd: #the trade is LONG
+		# 	return ("Long",round(target_risk_usd/(exit_usd - entry_usd - market_fee(entry_usd) - market_fee(exit_usd)),4))
+		# else: #the trade is SHORT
+		# 	return ("Short",round(target_risk_usd/(entry_usd - exit_usd - market_fee(entry_usd) - market_fee(exit_usd)),4))
+
+if __name__ == "__main__":
+
+	trade = Trade(5900,6000,5800,-3.30)
+
+	print(trade.get_position_size())
+	print(trade.get_dir())
+	print(trade.target_return())
+	print(trade.exit_return())
+	print(trade.get_rr())
+	print()
+
+	trade.update(5900,5800,6000,-3.30)
+
+	print(trade.get_position_size())
+	print(trade.get_dir())
+	print(trade.target_return())
+	print(trade.exit_return())
+	print(trade.get_rr())
+	
+	#print("short risk = $3.30 entry = 5900, exit = 6000")
+	#print(get_position_size(-3.3,5900,6000))
+	#print("long risk = $3.30 entry = 5900, exit = 5800")
+	#print(get_position_size(-3.3,5900,5800))
--- ws4.py
+++ ws4.py
@@ -1,10 +1,24 @@
 import websocket_manager
+#import trade_sizer as ts
 import tkinter as tk
+from tkinter import ttk
+import trade_sizer as ts
 
 ws = websocket_manager.WebsocketManager()
+
 window = tk.Tk()
+
 price_p = tk.Label(text=str(0))
 price_p.pack()
+
+target_var = tk.StringVar()
+exit_var = tk.StringVar()
+
+target_price = ttk.Entry(window, width = 10, textvariable = target_var)
+exit_price = ttk.Entry(window, width = 10, textvariable = exit_var)
+
+target_price.pack()
+exit_price.pack()
 
 def update():
 	price_p.config(text=str(ws.msg))
