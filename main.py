ffb5be248a8278eed0214f7415b053c18541b043
adoti <34661902+adoti@users.noreply.github.com>
2021-02-20 15:23:29 -0500

    changed GUI from pack() to grid()

--- main.py
+++ main.py
@@ -24,60 +24,90 @@ def change_exit(*args):
 	trade.update_exit(exit_var.get())
 
 def change_risk(*args):
-	print(f"risk% changed: {risk_percent_var.get()}")
-	print(f"risk$ updated: {risk_percent_var.get()*bankroll}")
-	trade.update_risk(risk_percent_var.get()*bankroll)
+	print(f"risk% changed: {risk_percent_var.get()}%")
+	print(f"risk$ updated: {risk_percent_var.get()*bankroll/100}")
+	trade.update_risk(risk_percent_var.get()*bankroll/100)
 
 ws = websocket_manager.WebsocketManager(tickers[0])
 
 window = tk.Tk()
 
-#labels
+#live price
+price_label = tk.Label(text="Price:")
+price_label.grid(row=0,column=0,sticky="E")
 price_p = tk.Label(text=str(0))
-price_p.pack()
-risk_p = tk.Label(text=str(0))
-risk_p.pack()
-reward_p = tk.Label(text=str(0))
-reward_p.pack()
-rr_p = tk.Label(text=str(0))
-rr_p.pack()
-position_p = tk.Label(text=str(0))
-position_p.pack()
-dir_p = tk.Label(text="")
-dir_p.pack()
+price_p.grid(row=0,column=1,sticky="W")
 
-#target, exit, & risk vars & traces
+#target
+target_label = tk.Label(text="Target:")
+target_label.grid(row=1,column=0,sticky="E")
 target_var = tk.DoubleVar()
-exit_var = tk.DoubleVar()
-risk_percent_var = tk.DoubleVar()
 target_var.trace("w",change_target)
+target_price = ttk.Entry(window, width = 10, textvariable = target_var)
+target_price.grid(row=1,column=1)
+
+#stoploss price
+exit_label = tk.Label(text="Stop Loss:")
+exit_label.grid(row=2,column=0,sticky="E")
+exit_var = tk.DoubleVar()
 exit_var.trace("w",change_exit)
+exit_price = ttk.Entry(window, width = 10, textvariable = exit_var)
+exit_price.grid(row=2,column=1)
+dir_p = tk.Label(text="")
+dir_p.grid(row=2,column=2)
+
+
+#live R
+rr_label = tk.Label(text="R/R:")
+rr_label.grid(row=3,column=0,sticky="E")
+rr_p = tk.Label(text=str(0))
+rr_p.grid(row=3,column = 1,sticky="W")
+
+#Bankroll
+br_label = tk.Label(text="Bankroll:")
+br_label.grid(row=4,column=0,sticky="E")
+br_p = tk.Label(text="$"+str(ts.round_d(bankroll,2)))
+br_p.grid(row=4,column=1,sticky="W")
+
+#risk%
+risk_label = tk.Label(text="Risk %")
+risk_label.grid(row=5,column=0,sticky="E")
+risk_percent_var = tk.DoubleVar()
 risk_percent_var.trace("w",change_risk)
+risk_percent = ttk.Entry(window, width = 10, textvariable = risk_percent_var)
+risk_percent.grid(row=5,column=1)
+
+#risk usd
+risku_label = tk.Label(text="Risk $/Reward $")
+risku_label.grid(row=6,column=0,sticky="E")
+risk_p = tk.Label(text=str(0))
+risk_p.grid(row=6,column=1,sticky="W")
+reward_p = tk.Label(text=str(0))
+reward_p.grid(row=6,column=2,sticky="W")
+
+#position size
+position_label = tk.Label(text="Position Size:")
+position_label.grid(row=7,column=0,sticky="E")
+position_p = tk.Label(text=str(0))
+position_p.grid(row=7,column=1,sticky="W")
 
 #tickers dropdown - create var, trace, default, and option menu
 tickers_var = tk.StringVar()
 tickers_var.trace("w",change_subscription)
 tickers_var.set(tickers[0])
 ticker_dropdown = tk.OptionMenu(window,tickers_var,*tickers)
-ticker_dropdown.pack()
+ticker_dropdown.grid(row=0, column=2)
 
-#target, exit, & risk fields + pack
-target_price = ttk.Entry(window, width = 10, textvariable = target_var)
-exit_price = ttk.Entry(window, width = 10, textvariable = exit_var)
-risk_percent = ttk.Entry(window, width = 10, textvariable = risk_percent_var)
-target_price.pack()
-exit_price.pack()
-risk_percent.pack()
 
 def update():
 	trade.update_entry(ws.msg)
 	trade.set_dir()
-	price_p.config(text="price: $" + str(ws.msg))
-	risk_p.config(text="risk: $" + str(trade.exit_return()))
-	reward_p.config(text="reward: $" + str(trade.target_return()))
-	rr_p.config(text="R/R: " + str(trade.get_rr()))
-	position_p.config(text="position size: " + str(trade.get_position_size()))
-	dir_p.config(text="direction: " + str(trade.get_dir()))
+	price_p.config(text=str(ws.msg))
+	risk_p.config(text="$"+str(trade.exit_return()))
+	reward_p.config(text="$"+str(trade.target_return()))
+	rr_p.config(text=str(trade.get_rr()))
+	position_p.config(text=str(trade.get_position_size()))
+	dir_p.config(text=str(trade.get_dir()))
 	window.after(200,update)
 
 def init():
